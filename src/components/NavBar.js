b05db8e8951d5d187523265ae46a995bfeda7301
Kapil Jadhav <103952813+Kapil56J@users.noreply.github.com>
2022-09-30 11:41:51 +0530

    Update NavBar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,17 @@ import React from 'react';
 
 export const NavBar = () =>{
     return(
+          <nav>
+      <ul>
+          <li >
+            <Link to="/home" className="home-link">Home Page</Link>
+          </li>
+        <li>
+          <Link to="/"  className="index-link">Index Page</Link>
+        </li>
+      </ul>
+    </nav>
 
     )
 }
+export default NavBar;
